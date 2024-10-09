713aa08ad5e98733429f6908b0a0a2e7cdf062d6
Braden Tierney <braden@bt-hms-mbp.harvardsecure.wireless.med.harvard.edu>
2024-10-09 09:51:28 -0400

    setup update

--- setup.py
+++ setup.py
@@ -4,15 +4,19 @@ setup(
     name='magus',
     version='0.1.0',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
-        # List your dependencies here
+        # Add dependencies here, e.g., 'numpy', 'pandas'
     ],
     entry_points={
         'console_scripts': [
-            'magus-taxonomy=magus.taxonomy:main',
-            'magus-assemble=magus.assemble:main',
-            'magus-resolvegenomes=magus.resolvegenomes:main',
-            'magus-genelevel=magus.genelevel:main',
+            'magus=magus.qc:main',
+            'magus=magus.single_assembly:main',
+            'magus=magus.single_binning:main',
+            'magus=magus.cluster_contigs:main',
+            'magus=magus.coassembly_binning:main',
+            'magus=magus.coassembly:main',
+            'magus=magus.taxonomy:main',
         ],
     },
 )
