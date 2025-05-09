ed0753f9be5ee3ab7741165942354b5a4aebc9e5
Tierney <brt448@compute-a-16-28.o2.rc.hms.harvard.edu>
2025-05-09 09:32:34 -0400

    restore main

---
+++ magus/magus_main.py
@@ -0,0 +1,46 @@
+import sys
+import subprocess
+import os
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: magus <command> [<args>]")
+        sys.exit(1)
+
+    command = sys.argv[1]
+    args = sys.argv[2:]
+
+    # Mapping commands to scripts
+    command_map = {
+        'qc': 'qc.py',
+        'assemble-hosts':'assemble-host.py',
+        'subsample-reads':'subsample-reads.py',
+        'taxonomy': 'taxonomy.py',
+        'cluster-contigs': 'cluster-contigs.py',
+        'single-assembly': 'single-assembly.py',
+        'single-binning': 'single-binning.py',
+        'cluster-contigs': 'cluster-contigs.py',
+        'coassembly': 'coassembly.py',
+        'coassembly-binning': 'coassembly-binning.py',
+        'dereplicate': 'dereplicate-genomes.py',
+        'find-viruses': 'find-viruses.py',
+        'find-euks': 'find-euks.py',
+        'finalize-bacterial-mags': 'finalize-bacterial-mags.py',
+        'find-genes':'find_genes.py'
+  }
+
+    # Check for valid command
+    if command not in command_map:
+        print(f"Unknown command: {command}")
+        print("Available commands:", ", ".join(command_map.keys()))
+        sys.exit(1)
+
+    # Get the absolute path to the script inside the package
+    script = command_map[command]
+    script_path = os.path.join(os.path.dirname(__file__), script)
+
+    # Run the appropriate script
+    subprocess.run(['python', script_path] + args)
+
+if __name__ == '__main__':
+    main()
